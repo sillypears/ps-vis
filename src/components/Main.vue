bfee9bc60fa1096c2f7d5ec2eadfd13370c64853
sillypears <sillypears@users.noreply.github.com>
2022-08-17 09:29:38 -0400

    We do a little reversing

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -11,12 +11,14 @@
                 lg="2"
                 md="3"
                 sm="4"
-                v-for="(day, key) in calData" 
+                v-for="(day, key) in Object.keys(calData).slice().reverse()" 
                 :key="'day-' + key "
+
             >
             <b-card-group 
                 deck
                 style="background-color: lightgrey;"
+                
             >
                 <b-card
                     no-body
@@ -26,27 +28,27 @@
                     <b-card-header
                         header-bg-variant="primary"
                         header-text-variant="white"
-                        :title="day['sculpts'][0].date_day"
+                        :title="calData[day]['sculpts'][0].date_day"
                     >
-                        Day {{ key }}
+                        Day {{ calData[day]['sculpts'][0].day }}
                     </b-card-header>
                     <b-card-body>
                         <div class="text-left">
                             Entry:
                         </div>
-                        <div class="text-right" v-if="day['sculpts'][0].entry_time < 12">
-                            {{ day['sculpts'][0].entry_time }}AM PST
+                        <div class="text-right" v-if="calData[day]['sculpts'][0].entry_time < 12">
+                            {{ calData[day]['sculpts'][0].entry_time }}AM PST
                         </div>
                         <div class="text-right" v-else>
-                            {{ day['sculpts'][0].entry_time - 12}}PM PST
+                            {{ calData[day]['sculpts'][0].entry_time - 12}}PM PST
                         </div>
                         <div class="text-right" >
-                            {{ day['sculpts'][0].entry_window}} {{day['sculpts'][0].entry_unit}}<span v-if="day['sculpts'][0].entry_window != 1">s</span>
+                            {{ calData[day]['sculpts'][0].entry_window}} {{calData[day]['sculpts'][0].entry_unit}}<span v-if="calData[day]['sculpts'][0].entry_window != 1">s</span>
                         </div>
                         <div class="text-left">
                             Colorway:
                         </div>
-                        <div v-for="(c,i) in day['colorways']" :key="i + '-' + day['sculpts'][0].day + '-' + c">
+                        <div v-for="(c,i) in calData[day]['colorways']" :key="i + '-' + calData[day]['sculpts'][0].day + '-' + c">
                             <div class="text-right">
                                 {{ c }}
                             </div>
@@ -54,7 +56,7 @@
                         <div class="text-left">
                             Sculpts:
                         </div>
-                        <div v-for="(s,i) in day['sculpts']" :key="i + '-'+ s.day + '-' + s.sculpt + '-' + s.colorway">
+                        <div v-for="(s,i) in calData[day]['sculpts']" :key="i + '-'+ s.day + '-' + s.sculpt + '-' + s.colorway">
                             <div class="text-right">
                                 {{ s.sculpt }} - {{ s.quantity }}
                             </div>
@@ -63,10 +65,10 @@
                     <b-card-footer >
                         <b-row>
                             <b-col class="col-md-2">
-                                <img v-if="day['sculpts'][0].goldbag" title="Goldbags are back :o" alt="Goldbags are back :o" src="../assets/goldbag.png" height="24px" />
+                                <img v-if="calData[day]['sculpts'][0].goldbag" title="Goldbags are back :o" alt="Goldbags are back :o" src="../assets/goldbag.png" height="24px" />
                             </b-col>
                             <b-col class="col-md-10 text-right">
-                                <a class="" :href="day['sculpts'][0].link" target="_blank"><img title="Instagram post" alt="Link to post on Instagram" src="../assets/ig.png" height="24px"></a>
+                                <a class="" :href="calData[day]['sculpts'][0].link" target="_blank"><img title="Instagram post" alt="Link to post on Instagram" src="../assets/ig.png" height="24px"></a>
                             </b-col>
                         </b-row>
                     </b-card-footer>
@@ -196,4 +198,5 @@ b-link {
     text-decoration: none;
     color:black;
 }
+
 </style>
